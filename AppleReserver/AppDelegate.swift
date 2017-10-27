021f6b62b6bd708a1ceaac32d1dc6f7014021a68
Sunnyyoung <iSunnyyoung@gmail.com>
2017-10-27 22:17:27 +0800

    Fixed reopen issue

--- AppleReserver/AppDelegate.swift
+++ AppleReserver/AppDelegate.swift
@@ -11,12 +11,14 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-    }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
+        let window = sender.windows.first
+        if flag {
+            window?.orderFront(sender)
+        } else {
+            window?.makeKeyAndOrderFront(sender)
+        }
+        return true
     }
 
 }
