e6500f590e74f53742ce17dde3294468f3ce1f8d
Sunnyyoung <iSunnyyoung@gmail.com>
2021-09-17 21:09:18 +0800

    Update product model

--- AppleReserver/Module/Model/Product.swift
+++ AppleReserver/Module/Model/Product.swift
@@ -11,21 +11,6 @@ import Foundation
 struct Product: Codable {
     let partNumber: String
     let description: String
-    let color: String
     let capacity: String
-    let screenSize: String
     let price: String
-    let installmentPrice: String
-    let installmentPeriod: String
-    let iUPPrice: String
-    let iUPInstallments: String
-    let colorSortOrder: String
-    let swatchImage: URL
-    let image: URL
-    let contractEnabled: Bool
-    let unlockedEnabled: Bool
-    let groupID: String
-    let groupName: String
-    let subfamilyID: String
-    let subfamily: String
 }
