0b5f5a5f74246508da7c8cfb74ee4d6a8a0abfdd
Martin Lau <martinlau@livemail.tw>
2021-12-08 09:50:46 +0800

    format source code

--- Sources/AppleReserver/Speech.swift
+++ Sources/AppleReserver/Speech.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by MartinLau on 26/11/2021.
 //
@@ -9,25 +9,25 @@ import Foundation
 import AppKit
 
 struct Speech {
-	private var speechSynthesizer: NSSpeechSynthesizer
-	
-	init() {
-		speechSynthesizer = NSSpeechSynthesizer()
-	}
-	
-	@discardableResult func startSpeaking(_ text: String, voiceLanguage: String? = nil) -> Bool {
-		if let voiceLanguage = voiceLanguage {
-			let availableVoices = NSSpeechSynthesizer.availableVoices
-			for v in availableVoices {
-				let arrt = NSSpeechSynthesizer.attributes(forVoice: v)
-				if let localeIdentifier = arrt[.localeIdentifier] as? String, localeIdentifier == voiceLanguage {
-					speechSynthesizer.setVoice(v)
-					break
-				}
-			}
-		}
-		return speechSynthesizer.startSpeaking(text)
-	}
+    private var speechSynthesizer: NSSpeechSynthesizer
+    
+    init() {
+        speechSynthesizer = NSSpeechSynthesizer()
+    }
+    
+    @discardableResult func startSpeaking(_ text: String, voiceLanguage: String? = nil) -> Bool {
+        if let voiceLanguage = voiceLanguage {
+            let availableVoices = NSSpeechSynthesizer.availableVoices
+            for v in availableVoices {
+                let arrt = NSSpeechSynthesizer.attributes(forVoice: v)
+                if let localeIdentifier = arrt[.localeIdentifier] as? String, localeIdentifier == voiceLanguage {
+                    speechSynthesizer.setVoice(v)
+                    break
+                }
+            }
+        }
+        return speechSynthesizer.startSpeaking(text)
+    }
 }
 
 
